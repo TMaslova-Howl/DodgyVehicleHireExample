f1fa8a8929122257a1bb518401e7e537092e376b
TMaslova-Howl <tanyamaslova@gmail.com>
2022-10-06 20:08:33 +0100

    Add swagger documentation

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ testing framework for unit tests.
 This example uses in memory database and even a very hacky hardcoded AdminFees class that pretends to be a repository
 for the time saving porpoises.
 This example also contains a brief API tests that are written in Spock and JUnit.  
+
+To start this API locally just run it as a normal SpringBoot application in your IDE.
+You can access API documentation once you started the application on http://localhost:8080/swagger-ui/
--- src/main/java/com/tanya/maslova/vehiclehire/controller/VehicleHireController.java
+++ src/main/java/com/tanya/maslova/vehiclehire/controller/VehicleHireController.java
@@ -2,6 +2,8 @@ package com.tanya.maslova.vehiclehire.controller;
 
 import com.tanya.maslova.vehiclehire.model.Car;
 import com.tanya.maslova.vehiclehire.service.VehicleHireService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api
 @RestController
 @RequiredArgsConstructor
 public class VehicleHireController {
@@ -17,21 +20,25 @@ public class VehicleHireController {
     private final VehicleHireService vehicleHireService;
 
     @GetMapping("/availableCars")
+    @ApiOperation("Gets available cars for hire")
     public List<Car> getAvailableCars(){
         return vehicleHireService.getAllAvailableCars();
     }
 
     @GetMapping("/hiredCars")
+    @ApiOperation("Gets already hired cars")
     public List<Car> getHiredCars(){
         return vehicleHireService.getAllHiredCars();
     }
 
     @PostMapping("/calculateHireCost")
+    @ApiOperation("Calculates hire costs based on selected car types and date period")
     public int calculateHireCost(@RequestBody HiredCarsDto hiredCars){
         return vehicleHireService.calculateHireCost(hiredCars.getCarCategories(),hiredCars.getDateFrom(), hiredCars.getDateTo());
     }
 
     @PostMapping("/finalCost")
+    @ApiOperation("Calculates final cost")
     public int calculateFinalCost(@RequestBody HiredCarsDto hiredCars){
         return vehicleHireService.getHireCostWithAdminFees(hiredCars.getCarCategories(),hiredCars.getDateFrom(), hiredCars.getDateTo());
     }
